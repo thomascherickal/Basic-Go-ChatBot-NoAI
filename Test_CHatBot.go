731c60ac385b998ad002ece1a218403c78df51a4
Thomas Cherickal <thomascherickal@gmail.com>
2023-01-05 03:19:09 +0530

    First Commit

---
+++ Test_CHatBot.go
@@ -0,0 +1,23 @@
+module 'C:\OneDrive\My Projects\'
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(respondToGreeting("hi"))
+	fmt.Println(respondToGreeting("hello"))
+	fmt.Println(respondToGreeting("bye"))
+	fmt.Println(respondToGreeting("goodbye"))
+}
+
+func respondToGreeting(greeting string) string {
+	greeting = strings.ToLower(greeting)
+	if greeting == "hi" || greeting == "hello" {
+		return "Hello, User"
+	} else if greeting == "bye" || greeting == "goodbye" {
+		return "Goodbye, User"
+	}
+	return "I don't recognize that greeting."
+}
