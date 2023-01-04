4b4d8e5bb2c73c45f05991c3048ee01a8013f505
Thomas Cherickal <thomascherickal@gmail.com>
2023-01-05 03:30:15 +0530

    finished"

--- Test_CHatBot.go
+++ Test_CHatBot.go
@@ -1,23 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "testing"
 
-func main() {
-	fmt.Println(respondToGreeting("hi"))
-	fmt.Println(respondToGreeting("hello"))
-	fmt.Println(respondToGreeting("bye"))
-	fmt.Println(respondToGreeting("goodbye"))
-}
-
-func respondToGreeting(greeting string) string {
-	greeting = strings.ToLower(greeting)
-	if greeting == "hi" || greeting == "hello" {
-		return "Hello, User"
-	} else if greeting == "bye" || greeting == "goodbye" {
-		return "Goodbye, User"
+func TestRespondToGreeting(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{"hi", "Hello, User"},
+		{"hello", "Hello, User"},
+		{"bye", "Goodbye, User"},
+		{"goodbye", "Goodbye, User"},
+		{"something else", "I don't recognize that greeting."},
 	}
-	return "I don't recognize that greeting."
+	for _, test := range tests {
+		if got := TestRespondToGreeting(test.input); got != test.want {
+			t.Errorf("respondToGreeting(%q) = %q, want %q", test.input, got, test.want)
+		}
+	}
 }
